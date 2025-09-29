0ae8edd53b6f769fa345c0189e9b61d3edf0e9fd
flaviosalesv <flavio.salesv@gmail.com>
2025-09-29 13:54:09 -0300

    feat: add basic product registration with limit of 3 products

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 
 products_list = []
 
-products = {
+while len(products_list) < 3:
+    products = {
     "product": input("Product: "),
     "brand": input("Brand: "),
     "price": float(input("Price: ")),
     "expiration": input("Expiration date: ")
 }
 
+    products_list.append(products)
 
-products_list.append(products)
+    if len(products_list) < 3:
+        add_more = input("Do you want to add another product? (y/n): ")
+        if add_more != 'y':
+            break
+
 print(products_list)
 print(products["brand"])
